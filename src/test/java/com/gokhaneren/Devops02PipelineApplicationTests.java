dd4592b4fd61f0d452f746c985738227fc998b57
Gökhan Eren Kamer <gkhnrnkmr@gmail.com>
2025-08-24 12:18:19 +0300

    ilk demo proje kodları
    Demo projenin ilk kodlarını ekledik

---
+++ src/test/java/com/gokhaneren/Devops02PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhaneren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops02PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
